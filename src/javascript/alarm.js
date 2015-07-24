407bf47b9d98bfb0e3317f79fcc12da591e159ba
Julio <julio@acid.cl>
2015-07-24 14:19:12 -0300

    Minor fix identation

--- src/javascript/alarm.js
+++ src/javascript/alarm.js
@@ -1,12 +1,12 @@
-function reloj(){ 
-	var reloj = document.getElementById('reloj');
-	var hora = document.getElementById('hora');
-	var minuto = document.getElementById('minuto');
-	var segundo = document.getElementById('segundo');
-	var timezone = document.getElementById('timezone');
-	var m = moment();
+function reloj(){
+  var reloj = document.getElementById('reloj');
+  var hora = document.getElementById('hora');
+  var minuto = document.getElementById('minuto');
+  var segundo = document.getElementById('segundo');
+  var timezone = document.getElementById('timezone');
+  var m = moment();
 
-	//hora alarma ('0' + 4).slice(-2)
+  //hora alarma ('0' + 4).slice(-2)
   var horaValue = ('0' + hora.options[hora.selectedIndex].value).slice(-2);
   var minutoValue = ('0' + minuto.options[minuto.selectedIndex].value).slice(-2);
   var segundoValue = ('0' + segundo.options[segundo.selectedIndex].value).slice(-2);
@@ -18,44 +18,43 @@ function reloj(){
 
 
   if(strHoraAlarma === m.tz(tzValue).format("hh:mm:ss")){
-  	var snd = new Audio("vendor/wav/alarm.wav"); // buffers automatically when created
-  	snd.play();
+    var snd = new Audio("vendor/wav/alarm.wav"); // buffers automatically when created
+    snd.play();
   }
-	reloj.innerHTML = current; 
-  
+  reloj.innerHTML = current;
+
 }
 
 
 function populateArray(num,start){
-	var array = [];
-	for (var i = ((start == 0)?start:1); i <= num; i++) 
-		array.push(i);
-	return array;
+  var array = [];
+  for (var i = ((start == 0)?start:1); i <= num; i++)
+    array.push(i);
+  return array;
 }
 
 function populateSelect(select, langArray){
-	
-	for(element in langArray)
-	{
-	   var opt = document.createElement("option");
-	   opt.value = (typeof langArray[element] == "number") ? (('0' + langArray[element]).slice(-2)):langArray[element];
-	   opt.innerHTML = (typeof langArray[element] == "number") ? (('0' + langArray[element]).slice(-2)):langArray[element];
-	  select.appendChild(opt);
-	}
-} 
+
+  for(element in langArray)
+  {
+     var opt = document.createElement("option");
+     opt.value = (typeof langArray[element] == "number") ? (('0' + langArray[element]).slice(-2)):langArray[element];
+     opt.innerHTML = (typeof langArray[element] == "number") ? (('0' + langArray[element]).slice(-2)):langArray[element];
+    select.appendChild(opt);
+  }
+}
 
 +function () {
 
   document.addEventListener('DOMContentLoaded', function () {
-  	  	
-  	populateSelect(document.getElementById('hora'),populateArray(12));
 
-  	populateSelect(document.getElementById('minuto'),populateArray(59,0));
-  	populateSelect(document.getElementById('segundo'),populateArray(59,0));
-  	populateSelect(document.getElementById('timezone'),moment.tz.names());
-  	 //sound
+    populateSelect(document.getElementById('hora'),populateArray(12));
 
-  	setInterval(reloj, 1000);
+    populateSelect(document.getElementById('minuto'),populateArray(59,0));
+    populateSelect(document.getElementById('segundo'),populateArray(59,0));
+    populateSelect(document.getElementById('timezone'),moment.tz.names());
+
+    setInterval(reloj, 1000);
   });
 
 }();
